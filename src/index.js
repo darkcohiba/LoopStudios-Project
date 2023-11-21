fce89f457ca655508383ff56bea67cd785225122
Sam Waters <samuel.waters@gmail.com>
2023-11-21 14:40:38 -0700

    finished loopstudio

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './style.css'
 
+const menuButton = document.querySelector('#menu-btn')
+const menu = document.getElementById('menu')
+
+menuButton.addEventListener('click', navToggle)
+
+function navToggle(){
+    menuButton.classList.toggle('open')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
+}
